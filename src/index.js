d2a78f08fe03f9602d9cbedd9c2980d5da9ea56f
Lunik <guillaume.lunik@gmail.com>
2016-04-04 21:09:17 +0200

    standard style

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 var client = require('./client.js')
 var mega = require('./mega.js')
 
-function Mega_Torrent(data){
+function Mega_Torrent (data) {
   this.email = data.email
   this.password = data.password
+  this.status = null
 }
 
-Mega_Torrent.prototype.download = function(url){
+Mega_Torrent.prototype.download = function (url) {
   var self = this
-  setTimeout(function(){
+  setTimeout(function () {
     var c = new client()
-    c.download(url, function(torrent){
-      torrent.files.forEach(function(file){
+    c.download(url, function (torrent) {
+      torrent.files.forEach(function (file) {
         var m = new mega({email: self.email, password: self.password})
-        m.upload(file.name, function(){})
+        m.upload(file.name, function () {})
       })
     })
   }, 1)
--- src/mega.js
+++ src/mega.js
@@ -1,28 +1,28 @@
-var mega = require('mega');
+var mega = require('mega')
 
-var fs = require('fs');
+var fs = require('fs')
 
-function Mega(options){
-  this.storage = mega(options);
-  this.progress = 0;
+function Mega (options) {
+  this.storage = mega(options)
+  this.progress = 0
 }
 
-Mega.prototype.upload = function(file, callback){
-  var self = this;
-  setTimeout(function(){
-    var stream = fs.createReadStream("./downloads/"+file).pipe(
+Mega.prototype.upload = function (file, callback) {
+  var self = this
+  setTimeout(function () {
+    var stream = fs.createReadStream('./downloads/' + file).pipe(
       self.storage.upload(
-        file.split('\/')[file.split('\/').length -1]
+        file.split('\/')[file.split('\/').length - 1]
       )
-    );
+    )
     stream.on('data', (chunk) => {
       self.progress += chunk.length
-    });
+    })
 
-    stream.on('end'), function(){
+    stream.on('end', function () {
       callback()
-    });
-  }, 1);
+    })
+  }, 1)
 }
 
-module.exports = Mega;
+module.exports = Mega
