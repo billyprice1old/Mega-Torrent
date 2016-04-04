929aa6b11e5b7595909df26d6bb55b2753f4f8e2
Lunik <guillaume.lunik@gmail.com>
2016-04-04 21:18:20 +0200

    store clients and megas in module

--- src/index.js
+++ src/index.js
@@ -5,17 +5,23 @@ function Mega_Torrent (data) {
   this.email = data.email
   this.password = data.password
   this.status = 0
+  this.megas = []
+  this.clients = []
 }
 
 Mega_Torrent.prototype.download = function (url) {
   var self = this
   setTimeout(function () {
     var c = new client()
+    self.clients[url] = c
     c.download(url, function (torrent) {
+      delete self.clients[url]
       torrent.files.forEach(function (file) {
         var m = new mega({email: self.email, password: self.password})
+        self.megas[url] = m
         m.upload(file.name, function () {
           self.status = 0
+          delete self.megas[url]
         })
         self.status = 2
       })
