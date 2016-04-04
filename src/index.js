50112e6681d33607860a6c2999b716e0ca7c6bc3
Lunik <guillaume.lunik@gmail.com>
2016-04-04 21:04:16 +0200

    add async download

--- src/index.js
+++ src/index.js
@@ -8,13 +8,15 @@ function Mega_Torrent(data){
 
 Mega_Torrent.prototype.download = function(url){
   var self = this
-  var c = new client()
-  c.download(url, function(torrent){
-    torrent.files.forEach(function(file){
-      var m = new mega({email: self.email, password: self.password})
-      m.upload(file.name)
+  setTimeout(function(){
+    var c = new client()
+    c.download(url, function(torrent){
+      torrent.files.forEach(function(file){
+        var m = new mega({email: self.email, password: self.password})
+        m.upload(file.name)
+      })
     })
-  })
+  }, 1)
 }
 
 module.exports = Mega_Torrent
